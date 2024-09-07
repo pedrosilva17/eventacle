184b508bd5495d4299202081eb8197fce4ef0c38
Pedro Silva <pedroafsuper@gmail.com>
2024-09-07 01:16:04 +0100

    add format pre-commit hook

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -8,50 +8,48 @@ import UpdatePasswordForm from '@/Pages/Profile/Partials/UpdatePasswordForm.vue'
 import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm.vue';
 
 defineProps({
-    confirmsTwoFactorAuthentication: Boolean,
-    sessions: Array,
+	confirmsTwoFactorAuthentication: Boolean,
+	sessions: Array,
 });
 </script>
 
 <template>
-    <AppLayout title="Profile">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Profile
-            </h2>
-        </template>
+	<AppLayout title="Profile">
+		<template #header>
+			<h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Profile</h2>
+		</template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <div v-if="$page.props.jetstream.canUpdateProfileInformation">
-                    <UpdateProfileInformationForm :user="$page.props.auth.user" />
+		<div>
+			<div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+				<div v-if="$page.props.jetstream.canUpdateProfileInformation">
+					<UpdateProfileInformationForm :user="$page.props.auth.user" />
 
-                    <SectionBorder />
-                </div>
+					<SectionBorder />
+				</div>
 
-                <div v-if="$page.props.jetstream.canUpdatePassword">
-                    <UpdatePasswordForm class="mt-10 sm:mt-0" />
+				<div v-if="$page.props.jetstream.canUpdatePassword">
+					<UpdatePasswordForm class="mt-10 sm:mt-0" />
 
-                    <SectionBorder />
-                </div>
+					<SectionBorder />
+				</div>
 
-                <div v-if="$page.props.jetstream.canManageTwoFactorAuthentication">
-                    <TwoFactorAuthenticationForm
-                        :requires-confirmation="confirmsTwoFactorAuthentication"
-                        class="mt-10 sm:mt-0"
-                    />
+				<div v-if="$page.props.jetstream.canManageTwoFactorAuthentication">
+					<TwoFactorAuthenticationForm
+						:requires-confirmation="confirmsTwoFactorAuthentication"
+						class="mt-10 sm:mt-0"
+					/>
 
-                    <SectionBorder />
-                </div>
+					<SectionBorder />
+				</div>
 
-                <LogoutOtherBrowserSessionsForm :sessions="sessions" class="mt-10 sm:mt-0" />
+				<LogoutOtherBrowserSessionsForm :sessions="sessions" class="mt-10 sm:mt-0" />
 
-                <template v-if="$page.props.jetstream.hasAccountDeletionFeatures">
-                    <SectionBorder />
+				<template v-if="$page.props.jetstream.hasAccountDeletionFeatures">
+					<SectionBorder />
 
-                    <DeleteUserForm class="mt-10 sm:mt-0" />
-                </template>
-            </div>
-        </div>
-    </AppLayout>
+					<DeleteUserForm class="mt-10 sm:mt-0" />
+				</template>
+			</div>
+		</div>
+	</AppLayout>
 </template>
