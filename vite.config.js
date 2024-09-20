a4a2731be72226c50d9e23fbe8505aa08f6fe7e5
Pedro Silva <pedroafsuper@gmail.com>
2024-09-21 01:09:37 +0200

    add google material icons and their auto-importing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import components from 'unplugin-vue-components/vite';
 
 export default defineConfig({
 	plugins: [
+		components({
+			resolvers: [IconsResolver()],
+		}),
+		Icons({ compiler: 'vue3' }),
 		laravel({
 			input: 'resources/js/app.js',
 			ssr: 'resources/js/ssr.js',
